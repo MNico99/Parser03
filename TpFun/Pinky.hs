b82e6bfb8797522a9ecdc96fa3e96ed384977336
Usuario de Aplicaciones <lsiadmin@lsi.utn>
2019-11-06 17:37:12 -0300

    GG

--- TpFun/Pinky.hs
+++ TpFun/Pinky.hs
@@ -2,4 +2,16 @@ data Animal = Animal {
     iq :: Int,
     especie :: String,
     capacidades :: [String] 
-}
+} deriving Show
+
+leon = Animal 500 "Felino" ["Morder", "Atacar", "Ronrronear"]
+
+macri = Animal 2 "Felino" ["Aumentar el dolar", "Gato hay para rato"]
+
+ff = Animal 1 "Ave" ["No vuela", "Choriwonka", "Cristibook", "Manaos con pitusas"]
+
+inteligenciaSuperior :: Int -> Animal -> Animal
+inteligenciaSuperior n (Animal iq especie capacidades) = Animal (iq + n) especie capacidades
+
+pinkificar :: Animal -> Animal
+pinkificar (Animal iq especie _) = (Animal iq especie [])
